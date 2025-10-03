a8f7b969a5b295b267ddd8b5f1109a396d1d9127
gelutz <gabriel.lutz5@gmail.com>
2025-10-03 14:25:45 -0300

    feat: movendo pastas

---
+++ src/test/java/lutz/algasensors/processor/ProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package lutz.algasensors.processor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
