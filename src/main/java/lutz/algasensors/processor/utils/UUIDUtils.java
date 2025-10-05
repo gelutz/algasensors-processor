b32d4971029cc82aa873a6dfaaac2299e11888e1
gelutz <gabriel.lutz5@gmail.com>
2025-10-05 14:05:38 -0300

    test: correct test configuration + first test passing

---
+++ src/main/java/lutz/algasensors/processor/utils/UUIDUtils.java
@@ -0,0 +1,26 @@
+package lutz.algasensors.processor.utils;
+
+import com.fasterxml.uuid.Generators;
+
+import java.time.Instant;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.util.UUID;
+
+public class UUIDUtils {
+	private UUIDUtils() {}
+
+	public static java.util.UUID generateTimeBasedUUID() {
+		return Generators.timeBasedEpochRandomGenerator().generate();
+	}
+
+	public static OffsetDateTime extractOffsetDateTime(UUID uuid) {
+		if (uuid == null) {
+			return null;
+		}
+
+		long timestamp = uuid.getMostSignificantBits() >>> 16;
+
+		return OffsetDateTime.ofInstant(Instant.ofEpochMilli(timestamp), ZoneId.systemDefault());
+	}
+}
--- src/test/java/lutz/algasensors/processor/ProcessorApplicationTests.java
+++ src/test/java/lutz/algasensors/processor/ProcessorApplicationTests.java
@@ -1,13 +1,29 @@
 package lutz.algasensors.processor;
 
+import lutz.algasensors.processor.utils.UUIDUtils;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.OffsetDateTime;
+import java.time.temporal.ChronoUnit;
 
 @SpringBootTest
 class ProcessorApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void shouldGenerateUUIDv7() {
+		var uuid1 = UUIDUtils.generateTimeBasedUUID();
+
+		OffsetDateTime currentTime = OffsetDateTime
+				.now()
+				.truncatedTo(ChronoUnit.MINUTES);
+		OffsetDateTime uuidTime = UUIDUtils
+				.extractOffsetDateTime(uuid1)
+				.truncatedTo(ChronoUnit.MINUTES);
+
+
+		Assertions.assertEquals(uuidTime, currentTime);
 	}
 
 }
